1537b17a59a4c6be30b82e5dbf28a02cd1dab335
Mohammad Aghili <mhaghili1381@yahoo.com>
2023-11-28 23:49:38 +0330

    fix some bug in CommoditiesControllerTest

--- CA4/src/test/java/API/CommoditiesControllerTest.java
+++ CA4/src/test/java/API/CommoditiesControllerTest.java
@@ -2,29 +2,20 @@ package API;
 
 import application.BalootApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import controllers.CommoditiesController;
 import exceptions.NotExistentCommodity;
 import exceptions.NotExistentUser;
-import model.Commodity;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import org.springframework.test.web.servlet.MockMvc;
 import service.Baloot;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
